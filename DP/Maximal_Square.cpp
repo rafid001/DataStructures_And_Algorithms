09bc653024f1c75fb0412ee249bad2a955d6134d
rafid001 <hrafid001@gmail.com>
2023-08-27 14:33:20 +0530

    LL

---
+++ DP/Maximal_Square.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm>
+using namespace std;
+
+int solve(vector<vector<int>> matrix, int i, int j, int &maxi) {
+
+        if(i >= matrix.size() || j >= matrix[0].size()) {cout << "base reached "; return 0;}
+        
+        
+        int right = solve(matrix, i, j+1, maxi);
+        cout << "right here " << i << " " << j << " "<< right << endl;
+        int diagonal = solve(matrix, i+1, j+1, maxi);
+        cout << "diagonal here " << i<< " " << j << " " << diagonal << endl;
+        int down = solve(matrix, i+1, j, maxi);
+        cout << "down here "<< i << " " << j << " " << down << endl;
+
+        cout << "I am i and j " << i << " " << j << ". " << "i am right, diag and down " << right << " " << diagonal << " " << down << endl;
+        if(matrix[i][j] == 1) {
+            int ans = 1 + min(right, min(diagonal, down));
+            maxi = max(maxi, ans);
+            return ans;
+        }
+        else {
+            cout << "reached else" << endl;
+            return 0;
+        }
+}
+
+int main() {
+    
+    vector<vector<int>> matrix = {
+        {{0,1},{1,0}}
+    };
+
+    int maxi = 0;
+
+    solve(matrix, 0, 0, maxi);
+    cout << endl;
+    cout << endl;
+    cout << maxi*maxi;
+    return 0;
+
+}
---
+++ Linked List/Reverse_LL.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int len(ListNode* head) {
+        
+        if(head == NULL) return 0;
+        if(head -> next == NULL) return 1;
+        int count = 0;
+
+        while(head != NULL) {
+            count++;
+            head = head -> next;
+        }
+        return count;
+
+    }
+
+    ListNode* solve(ListNode* head, int k, int size) {
+
+        if(size < k) 
+            return head;
+
+        if(head == NULL)
+            return head;
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+        int c = 0;
+
+        while(curr != NULL && c < k) {
+            forward = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = forward;
+            c++;
+        } 
+
+        if(forward != NULL) {
+            head -> next = solve(forward, k, size-k);
+        }
+
+        return prev;
+        
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int n = len(head);
+        return solve(head, k, n);
+    }
+};
